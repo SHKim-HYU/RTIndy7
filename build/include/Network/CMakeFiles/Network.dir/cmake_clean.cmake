5cb44f827056b2d40178019b5e55a6d41b34ac33
SHKim-HYU <tjsghd101@naver.com>
2023-06-09 19:42:07 +0900

    FT CB & Tool

--- build/include/Network/CMakeFiles/Network.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libNetwork.a"
-  "libNetwork.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Network.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
